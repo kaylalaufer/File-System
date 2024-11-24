2c42bd7b39be9e6f685e76587411f836309ff90d
Kayla Laufer <kaylauf618@gmail.com>
2024-11-24 00:31:32 -0500

    Added read block function in disk_manager. Added google test and make file

---
+++ build/CMakeFiles/runTests.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/kaylalaufer/Documents/Fordham/Operating Systems/Final_Project/disk_manager.cpp" "CMakeFiles/runTests.dir/disk_manager.cpp.o" "gcc" "CMakeFiles/runTests.dir/disk_manager.cpp.o.d"
+  "/Users/kaylalaufer/Documents/Fordham/Operating Systems/Final_Project/test_main.cpp" "CMakeFiles/runTests.dir/test_main.cpp.o" "gcc" "CMakeFiles/runTests.dir/test_main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/disk_manager.cpp.o"
+  "CMakeFiles/runTests.dir/disk_manager.cpp.o.d"
+  "CMakeFiles/runTests.dir/test_main.cpp.o"
+  "CMakeFiles/runTests.dir/test_main.cpp.o.d"
+  "runTests"
+  "runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
